e997ef3238f7d4d60710fc8303682e987ff8e77d
shrilaxmi <aithalshrilaxmi@rediffmail.com>
2016-06-29 18:06:01 +0530

    get users from the file

---
+++ rest.js
@@ -0,0 +1,33 @@
+
+	
+	var express = require('express');
+	var bodyParser  =  require("body-parser");
+	var fs = require("fs");
+	var app = express();
+
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+
+	/*	
+		creating the common route 
+		var router = express.Router();    
+		app.use('/api', router);
+	*/
+
+	app.get('/users/:id?', function (req, res) {
+		fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
+			val = JSON.parse(data);
+			if(req.params.id) {
+				console.log("id :"+req.params.id);
+			  	console.log( val[req.params.id]);
+			  	res.send(val[req.params.id]);
+			} else  {
+		   		console.log( val);
+		   		res.send(val);
+		   }
+		});
+	})
+
+	app.listen(8081);
+
+  
